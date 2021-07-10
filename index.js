68564d4870ba4fb1da8deda61c3a61009e0f4d55
Joshua Chosay <jjchosay@gmail.com>
2021-07-10 17:13:38 -0500

    Initial datatype skeletons created.

--- index.js
+++ index.js
@@ -1,4 +1,115 @@
 const inquirer = require('inquirer')
 const fs = require('fs')
+const jest = require('jest')
 
-console.log("Hello!")
+
+function askForManagerInfo(){
+    inquirer.prompt([
+        {
+            type: 'input',
+            message: "What is the team manager's name?",
+            name: 'empname',
+        },
+        {
+            type: 'input',
+            message: "What is the team manager's ID number?",
+            name: 'id',
+        },
+        {
+            type: 'input',
+            message: "What is the team manager's email address?",
+            name: 'email',
+        },
+        {
+            type: 'input',
+            message: "What is the team manager's office number?",
+            name: 'office',
+        }
+    ])
+    .then((answers) => {
+        addAnEmployee();
+    });
+}
+
+function askForEngineerInfo(){
+    inquirer.prompt([
+        {
+            type: 'input',
+            message: "What is your engineer's name?",
+            name: 'empname',
+        },
+        {
+            type: 'input',
+            message: "What is your engineer's ID number?",
+            name: 'id',
+        },
+        {
+            type: 'input',
+            message: "What is your engineer's email address?",
+            name: 'email',
+        },
+        {
+            type: 'input',
+            message: "What is your engineer's GitHub user name?",
+            name: 'github',
+        }
+    ])
+    .then((answers) => {
+        addAnEmployee();
+    });
+}
+
+function askForInternInfo(){
+    inquirer.prompt([
+        {
+            type: 'input',
+            message: "What is your intern's name?",
+            name: 'empname',
+        },
+        {
+            type: 'input',
+            message: "What is your intern's ID number?",
+            name: 'id',
+        },
+        {
+            type: 'input',
+            message: "What is your intern's email address?",
+            name: 'email',
+        },
+        {
+            type: 'input',
+            message: "What school does your intern attend?",
+            name: 'school',
+        }
+    ])
+    .then((answers) => {
+        addAnEmployee();
+    });
+}
+
+function addAnEmployee(){
+    inquirer.prompt([
+        {
+            type: 'list',
+            message: 'What type of team member would you like to add?',
+            name: 'emptype',
+            choices: ['Engineer','Intern','Finished adding team members'],
+        }
+    ])
+    .then((answers) => {
+        if (answers.emptype === "Engineer"){
+            askForEngineerInfo();
+        }else if (answers.emptype === "Intern"){
+            askForInternInfo();
+        }else{
+            console.log("Thanks for playing, guy!");
+        }
+    })
+}
+
+function init(){
+    console.log("Hello! Welcome to Team Builder 5000, a product of FRUOsoft.");
+    askForManagerInfo();
+}
+
+init();
---
+++ test/Employee.test.js
@@ -0,0 +1,14 @@
+describe("name of package here", () => {
+    describe("name of method or process", () => {
+      it("description of specific test", () => {
+        // arrange
+        // act
+        // assert
+      });
+      it("description of specific test", () => {
+        // arrange
+        // act
+        // assert
+      });
+    })
+  })
